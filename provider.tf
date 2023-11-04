b42e6219633317c5ae2713968844568714456b66
maheshwari game <126004119+Mahi2213game@users.noreply.github.com>
2023-11-04 17:54:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.10.0"
+      version = "~> 5.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region     = "us-west-1"
+  region = "us-west-1"
 }
